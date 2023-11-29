eb073e1fd6544358095b471e25fae212dcd8ff12
Hari Acharya <hari.blacktech@gmail.com>
2023-11-30 03:27:20 +0545

    login/logout func added

--- src/hooks/UseAuth.tsx
+++ src/hooks/UseAuth.tsx
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
 import { AuthUserType } from "../../types/AuthUserTypes";
 import { toASCII } from "punycode";
+import { toast } from "react-toastify";
 
 function UseAuth() {
-  const [account, setAccount] = useState<AuthUserType | null>(null);
+  let auth = null;
+  if (localStorage.getItem("_auth")) {
+    auth = JSON.parse(localStorage.getItem("auth") || "");
+  }
+  const [account, setAccount] = useState<AuthUserType | null>(auth);
 
   //provide current user status
   const currentUser = () => {
@@ -12,6 +17,10 @@ function UseAuth() {
 
   //login a user
   const login = async (email: string, password: string) => {
+    if (account?.email) {
+      return toast.error("User is already logged in.");
+    }
+
     try {
       const request = await fetch(process.env.BACKEND_API + "/auth/login", {
         method: "POST",
@@ -23,30 +32,41 @@ function UseAuth() {
           password,
         }),
       });
+
       //if successful
       if (request.status == 200) {
         const data = await request.json();
         localStorage.setItem("_auth", JSON.stringify(data));
-        setAccount(data);
+        return setAccount(data);
       }
 
       //if not found
       if (request.status == 404) {
-        //no user with that email or password.
+        return toast.error("No user found with that email");
       }
 
+      //some client error
       if (request.status == 401) {
-        //some client error
         const data = await request.json();
+        return toast.error(data.msg);
       }
-
-      return false;
+      return toast.error("An error while login, please try again later.");
     } catch (e) {
-      return false;
+      return toast.error("Please check your network connection.");
     }
   };
 
-  return {};
+  const logout = async () => {
+    const request = await fetch(process.env.BACKEND_API + "/auth/logout");
+    if (request.status === 200) {
+      toast.success("You have successfully signed out.");
+      localStorage.removeItem("_auth");
+    } else {
+      toast.error("You may not have completely signed out.");
+    }
+  };
+
+  return { login, currentUser, logout };
 }
 
 export default UseAuth;
